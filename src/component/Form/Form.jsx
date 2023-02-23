767e14c22c3ff680eadc7f3b2220fb203fef0410
blake S <design.harls@icloud.com>
2023-02-23 14:03:55 +0100

    finished

--- src/component/Form/Form.jsx
+++ src/component/Form/Form.jsx
@@ -16,16 +16,16 @@ function MyForm() {
 
  
   return (
-    <form class="wrapper" onSubmit={handleSubmit(onSubmit)}>
-        <h1 class="wrapper__title">Formulario</h1>
-      <fieldset class="wrapper__content">
+    <form className="wrapper" onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="wrapper__title">Formulario</h1>
+      <fieldset className="wrapper__content">
         <input id="nombre" placeholder="Nombre:" {...register("nombre", { required: true })} />
         {errors.nombre && <span>Debe rellenar este campo</span>}
 
         <input id="correo"  placeholder="Correo"  {...register("correo", { required: true })} />
         {errors.correo && <span>Debe rellenar este campo</span>}
         {
-          <select class="wrapper__select" id="categoria" name="dropdown">
+          <select className="wrapper__select" id="categoria" name="dropdown">
             <option value="">Elige la Categoria</option>
             <option value="1">Desarrollo Web</option>
             <option value="2">Ilustrador</option>
@@ -35,13 +35,13 @@ function MyForm() {
           </select>
         }
 
-        <input id="descripcion" placeholder="Descripción" {...register("description", { required: true })}/>
+        <textarea id="descripcion"  placeholder="Descripción" {...register("description", { required: true })}></textarea>
         {errors.description && <span>Debe rellenar este campo</span>}
       </fieldset>
     
-      <div class="wrapper__buttons">
-      <input  class="submit" id="submit" type="submit" value="ENVIAR" />
-      <input class="reset" id="reset" type="reset" value="RESTABLECER" />
+      <div className="wrapper__buttons">
+      <input  className="submit" id="submit" type="submit" value="ENVIAR" />
+      <input className="reset" id="reset" type="reset" value="RESTABLECER" />
 
       </div>
     </form>
