ed95af657b334d84be0cd5dbf6fa04ac12fde840
Sergi <sergialsina81@gmail.com>
2023-02-23 09:12:13 +0100

    rama boton mas componente

---
+++ src/component/Button.jsx
@@ -0,0 +1,11 @@
+import Button from 'bootstrap';
+
+const ButtonComponent = () => {
+  return (
+    <>
+      <Button variant="info" size="lg">Contactar con el anunciante</Button>
+    </>
+  );
+}
+
+export default ButtonComponent;
