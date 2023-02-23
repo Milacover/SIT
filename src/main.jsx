713a8ba99729a2862a7d38b27609fadb30585427
Milacover <117834108+Milacover@users.noreply.github.com>
2023-02-23 13:56:24 +0100

    add searchButton

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './app.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import DetailedView from './views/DetailedView';
+import SearchButton from './component/SearchButton';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+<SearchButton/>
     <DetailedView />
   </React.StrictMode>,
 )
